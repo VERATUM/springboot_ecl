21f5c757dde31d81911a4240c7b407211ebd6511
veratum <veratum@163.com>
2019-01-22 12:03:43 +0800

    My ecl to commit to springboot

---
+++ src/test/java/com/ver/SpringbootEclApplicationTests.java
@@ -0,0 +1,17 @@
+package com.ver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootEclApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
